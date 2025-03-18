d91837e3aa93e1edba33f611e109e9e25e9e2ba9
Abdelrahman Mohamed Ali <abdo.kaka538@gmail.com>
2025-03-18 23:16:11 +0200

    creating homeview file and adding assets folder

---
+++ lib/views/homeview.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Homeview extends StatelessWidget {
+  const Homeview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Image(image: AssetImage(assetName))
+        ],
+      ),
+    );
+  }
+}
